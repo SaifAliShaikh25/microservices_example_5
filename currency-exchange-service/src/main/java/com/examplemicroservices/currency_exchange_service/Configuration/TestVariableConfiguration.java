0c09e34c91d6ab726a701d5d92b8ea814073a839
Saif <saifali.shaikh2509@gmail.com>
2025-08-27 20:39:04 +0530

    Updated properties - Added plugin to create image using spring boot

--- currency-exchange-service/src/main/java/com/examplemicroservices/currency_exchange_service/Configuration/TestVariableConfiguration.java
+++ currency-exchange-service/src/main/java/com/examplemicroservices/currency_exchange_service/Configuration/TestVariableConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("limits-service")
+@ConfigurationProperties(prefix = "limits-service")
 @Getter
 @Setter
 @AllArgsConstructor
